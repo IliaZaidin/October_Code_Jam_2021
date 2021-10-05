524c47b938a04c0358cf9f45900883dfca232602
iliazaidin <ilia.zaid@gmail.com>
2021-10-05 12:59:55 +0300

    feat/ popup subtitle added

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //============================= Popup =======================
 const popup = document.querySelector('.popup');
+const popupSubtile = document.querySelector('.popup__subtitle');
 const popupSubmit = document.querySelector('.popup__submit');
 const popupClose = document.querySelector('.popup__close');
 const cardsButtonAll = document.querySelectorAll('.cards__button');
 
 cardsButtonAll.forEach(element => {
   element.addEventListener('click', (event) => { //open form
-    let eventTrigger = event.target.classList.value;
-    console.log(eventTrigger);
+    popupSubtile.textContent = event.target.textContent;
     popup.classList.add('popup_is_opened');
   });
 });
