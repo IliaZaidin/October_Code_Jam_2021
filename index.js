fd87907e03b68d39c83f47482113249121f0237e
iliazaidin <ilia.zaid@gmail.com>
2021-10-07 13:40:26 +0300

    feat/ implement design

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //============================= Popup =======================
 const popup = document.querySelector('.popup');
-const popupSubtile = document.querySelector('.popup__subtitle');
+const popupTitile = document.querySelector('.popup__title');
 const popupSubmit = document.querySelector('.popup__submit');
 const popupClose = document.querySelector('.popup__close');
 const cardsButtonAll = document.querySelectorAll('.cards__button');
 
 cardsButtonAll.forEach(element => {
   element.addEventListener('click', (event) => { //open form
-    popupSubtile.textContent = event.target.textContent;
+    popupTitile.textContent = event.target.textContent;
     popup.classList.add('popup_is_opened');
   });
 });
