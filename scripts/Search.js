2f00fc6a55a6766afe6fcd2dc5c27248831d7bda
Phoenix801 <gilad1590@gmail.com>
2021-10-08 14:32:02 +0300

    feature: searching for card items now works

--- scripts/Search.js
+++ scripts/Search.js
@@ -2,17 +2,39 @@ function getCards(elementSelector) {
     return [...document.querySelectorAll(elementSelector)];
 }
 
-export function filterCards(searchValue, elementSelector, elementTitleSelector) {
+function getCardsListItems(card, elementListItemSelector) {
+    return [...card.querySelectorAll(elementListItemSelector)]
+}
+
+export function filterCards(searchValue, elementSelector, elementTitleSelector, elementListItemSelector) {
     const cards = getCards(elementSelector);
     const inclusiveSearchValue = searchValue.toUpperCase(); //make search value not case sensitive
 
     cards.forEach(card => {
         const cardValue = card.querySelector(elementTitleSelector).textContent.toUpperCase();
+        const cardListItems = getCardsListItems(card, elementListItemSelector);
+        let cardsSearchedItemsCounter = 0; 
         if (cardValue.indexOf(inclusiveSearchValue) > -1) {
             card.style.display = "";
+            cardListItems.forEach(listItem => {
+                listItem.style.display = "";
+            })
         }
         else { 
-            card.style.display = "none";
+            cardListItems.forEach(listItem => {
+                const itemValue = listItem.textContent.toUpperCase();
+                if (itemValue.indexOf(inclusiveSearchValue) > -1) {
+                    card.style.display = "";
+                    listItem.style.display = "";
+                    cardsSearchedItemsCounter++;
+                }
+                else {
+                    listItem.style.display = "none";
+                }
+            })
+            if (!cardsSearchedItemsCounter) {
+                card.style.display = "none";
+            }
         }
     });
 }
