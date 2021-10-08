36abc668331e0ce70409f1a61a1809fec92438c4
iliazaidin <ilia.zaid@gmail.com>
2021-10-08 14:23:55 +0300

    fix/ submit popup

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 //============================= Popup =======================
 const popup = document.querySelector('.popup');
-const popupTitile = document.querySelector('.popup__title');
+const popupTitle = document.querySelector('.popup__title');
+const popupSubtitle = document.querySelector('.popup__subtitle');
 const popupSubmit = document.querySelector('.popup__submit');
 const popupClose = document.querySelector('.popup__close');
 const cardsButtonAll = document.querySelectorAll('.cards__button');
+const submittedInputAll = document.querySelectorAll('.popup__input');
+const popupWrapper = document.querySelector('.popup__wrapper');
 
 cardsButtonAll.forEach(element => {
   element.addEventListener('click', (event) => { //open form
-    popupTitile.textContent = event.target.textContent;
+    popupTitle.textContent = event.target.textContent;
     popup.classList.add('popup_is_opened');
   });
 });
- 
+
 popupSubmit.addEventListener('click', ()=> {  //submit request
-  popup.classList.remove('popup_is_opened');
+  submittedInputAll.forEach ((element) => {
+    element.remove();
+  })
+  popupSubtitle.textContent = "Your request has been received by the service and will be reviewed within seven calendar days. A message about the decision on your request will be sent to your email.   You will also receive supporting certificates that have the legal force of an ordinary paper document. Take care of yourself.";
+  popupSubmit.remove();
+  popupClose.textContent = "Return to home page";
+  popupWrapper.style.display = "flex";
+  popupWrapper.style.flexDirection = "column";
+  popupWrapper.style.gap = "50px";
 });
 
-popupClose.addEventListener('click', ()=> { //close form 
+popupClose.addEventListener('click', ()=> { //close form
+  location.reload();
   popup.classList.remove('popup_is_opened');
 })
 
