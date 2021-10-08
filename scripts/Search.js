6262fa02e335bc1e5b2a69fd2ddd942937f5fa7b
Phoenix801 <gilad1590@gmail.com>
2021-10-08 14:39:00 +0300

    refractor: added comments for additional search functionality

--- scripts/Search.js
+++ scripts/Search.js
@@ -14,26 +14,26 @@ export function filterCards(searchValue, elementSelector, elementTitleSelector, 
         const cardValue = card.querySelector(elementTitleSelector).textContent.toUpperCase();
         const cardListItems = getCardsListItems(card, elementListItemSelector);
         let cardsSearchedItemsCounter = 0; 
-        if (cardValue.indexOf(inclusiveSearchValue) > -1) {
-            card.style.display = "";
-            cardListItems.forEach(listItem => {
+        if (cardValue.indexOf(inclusiveSearchValue) > -1) { //if card is found within search
+            card.style.display = ""; //show the card
+            cardListItems.forEach(listItem => { //show all of it's items
                 listItem.style.display = "";
             })
         }
         else { 
             cardListItems.forEach(listItem => {
                 const itemValue = listItem.textContent.toUpperCase();
-                if (itemValue.indexOf(inclusiveSearchValue) > -1) {
-                    card.style.display = "";
-                    listItem.style.display = "";
-                    cardsSearchedItemsCounter++;
+                if (itemValue.indexOf(inclusiveSearchValue) > -1) { //if an item within a card is found
+                    card.style.display = ""; //show the card
+                    listItem.style.display = ""; //show the item
+                    cardsSearchedItemsCounter++; //for a later condition
                 }
                 else {
-                    listItem.style.display = "none";
+                    listItem.style.display = "none"; //if the item is not found within the card hide it
                 }
             })
-            if (!cardsSearchedItemsCounter) {
-                card.style.display = "none";
+            if (!cardsSearchedItemsCounter) { //if there isn't a single item found within the card
+                card.style.display = "none"; //hide the card
             }
         }
     });
